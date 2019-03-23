60dffd2c84269bd73d5d4ff5e4ca9a422ea1d1fb
Spriggs01 <sarah.spriggs@gamil.com>
2019-03-23 10:34:47 -0400

    tc:q

--- Princess_Bride/Program.cs
+++ Princess_Bride/Program.cs
@@ -10,6 +10,8 @@ namespace Princess_Bride
     {
         static void Main(string[] args)
         {
+            ProgramUI program = new ProgramUI();
+            program.Run();
         }
     }
 }
---
+++ Princess_Bride/ProgramUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Figgle;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Princess_Bride
+{
+ public   class ProgramUI
+    {
+        public void Run()
+       
+       
+        {
+
+            Console.WriteLine(@"(  _ \(  _ \(  )(  ( \ / __)(  __)/ ___)/ ___)  (  _ \(  _ \(  )(    \(  __) ");
+            Console.WriteLine(@" ) __/ )   / )( /    /( (__  ) _) \___ \\___ \   ) _ ( )   / )(  ) D ( ) _)  ");
+            Console.WriteLine(@"(__)  (__\_)(__)\_)__) \___)(____)(____/(____/  (____/(__\_)(__)(____/(____) ");
+            Console.WriteLine(@"  __   ____  _  _  ____  __ _  ____  _  _  ____  ____                        ");
+            Console.WriteLine(@" / _\ (    \/ )( \(  __)(  ( \(_  _)/ )( \(  _ \(  __)                       ");
+            Console.WriteLine(@"/    \ ) D (\ \/ / ) _) /    /  )(  ) \/ ( )   / ) _)                        ");
+            Console.WriteLine(@"\_/\_/(____/ \__/ (____)\_)__) (__) \____/(__\_)(____)                       ");
+
+            Console.ReadLine();
+        }
+
+    }
+}
